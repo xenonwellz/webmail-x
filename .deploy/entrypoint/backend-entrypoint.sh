31062ee768c7612fbabf3fe5dcb071b02ca33f5a
xenonwellz <59710311+xenonwellz@users.noreply.github.com>
2024-12-28 18:56:26 +0100

    fix deploy

--- .deploy/entrypoint/backend-entrypoint.sh
+++ .deploy/entrypoint/backend-entrypoint.sh
@@ -1,17 +1,4 @@
 #!/bin/sh
-
-# Wait for the database to be ready
-echo "Waiting for database to be ready..."
-while ! nc -z $DATABASE_HOST $DATABASE_PORT; do
-  sleep 1
-done
-echo "Database is ready!"
-
-# Run migrations
-echo "Running database migrations..."
-node /app/packages/backend/ace migration:run --force
-echo "Migrations completed!"
-
 # Start the application
 echo "Starting the application..."
 exec "$@"
